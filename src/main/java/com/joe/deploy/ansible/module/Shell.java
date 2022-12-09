9f6650c14c52d936cd7d9c966bab9c39dffa9ccf
zhangqi <zhangqi13c@163.com>
2022-12-09 17:16:34 +0800

    Add ansible module

--- src/main/java/com/joe/deploy/ansible/module/Shell.java
+++ src/main/java/com/joe/deploy/ansible/module/Shell.java
@@ -10,13 +10,61 @@ import lombok.Data;
 @Builder
 public class Shell {
 
+    /**
+     * Change into this directory before running the command.
+     */
     private String chdir;
+
+    /**
+     * The command to run followed by optional arguments.
+     */
     private String cmd;
+
+    /**
+     * A filename, when it already exists, this step will not be run.
+     */
     private String creates;
+
+    /**
+     * Change the shell used to execute the command.
+     * This expects an absolute path to the executable.
+     */
     private String executable;
+
+    /**
+     * The shell module takes a free form command to run, as a string.
+     * There is no actual parameter named ‘free form’.
+     * See the examples on how to use this module.
+     */
     private String freeForm;
+
+    /**
+     * A filename, when it does not exist, this step will not be run.
+     */
     private String removes;
+
+    /**
+     * Set the stdin of the command directly to the specified value.
+     */
     private String stdin;
+
+    /**
+     * Whether to append a newline to stdin data.
+     * Choices:  false  true(default)
+     */
     private boolean stdinAddNewline;
 
+    @Override
+    public String toString() {
+        return "Shell{" +
+                "chdir='" + chdir + '\'' +
+                ", cmd='" + cmd + '\'' +
+                ", creates='" + creates + '\'' +
+                ", executable='" + executable + '\'' +
+                ", freeForm='" + freeForm + '\'' +
+                ", removes='" + removes + '\'' +
+                ", stdin='" + stdin + '\'' +
+                ", stdinAddNewline=" + stdinAddNewline +
+                '}';
+    }
 }
--- src/main/java/com/joe/deploy/ansible/task/BaseTask.java
+++ src/main/java/com/joe/deploy/ansible/task/BaseTask.java
@@ -1,8 +1,11 @@
 package com.joe.deploy.ansible.task;
 
+import lombok.Data;
+
 /**
  * @author zhangqi
  */
+@Data
 public class BaseTask {
 
     private String name;
